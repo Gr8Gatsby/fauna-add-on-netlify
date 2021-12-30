5772ef7bd613635f1b287602e29cbbbcc11d3bae
Kevin Hill <kevinhill@live.com>
2021-12-29 19:02:07 -0800

    adding build step

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,47 +1,158 @@
-const create = async () => {
-  const url = "/.netlify/functions/create";
-  const createDB = await fetch(url, {
-    body: JSON.stringify({ name: "Todo-list", desc: "Patrick's mother" }),
-    method: "POST",
-    "Content-Type": "application/json",
-  });
-  const res = await createDB.json();
-  const data = res.body;
+"use strict";
 
-  console.log(data);
-};
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-const readAll = async () => {
-  const url = "/.netlify/functions/read";
-  const readNotes = await fetch(url);
-  const res = await readNotes.json();
-  const data = res.body;
+var create = function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    var url, createDB, res, data;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            url = "/.netlify/functions/create";
+            _context.next = 3;
+            return fetch(url, {
+              body: JSON.stringify({ name: "Todo-list", desc: "Patrick's mother" }),
+              method: "POST",
+              "Content-Type": "application/json"
+            });
 
-  console.log(data);
-};
+          case 3:
+            createDB = _context.sent;
+            _context.next = 6;
+            return createDB.json();
 
-const update = async () => {
-  const url = "/.netlify/functions/update";
-  const readNotes = await fetch(url, {
-    body: JSON.stringify({ name: "negative", desc: "Jesus's mother" }),
-    method: "PATCH",
-    "Content-Type": "application/json",
-  });
-  const res = await readNotes.json();
-  const data = res.body;
+          case 6:
+            res = _context.sent;
+            data = res.body;
 
-  console.log(data);
-};
 
-const deleteOne = async () => {
-  const url = "/.netlify/functions/delete";
-  const readNotes = await fetch(url);
-  const res = await readNotes.json();
-  const data = res.body;
+            console.log(data);
 
-  console.log(data);
-};
+          case 9:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined);
+  }));
 
+  return function create() {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+var readAll = function () {
+  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2() {
+    var url, readNotes, res, data;
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            url = "/.netlify/functions/read";
+            _context2.next = 3;
+            return fetch(url);
+
+          case 3:
+            readNotes = _context2.sent;
+            _context2.next = 6;
+            return readNotes.json();
+
+          case 6:
+            res = _context2.sent;
+            data = res.body;
+
+
+            console.log(data);
+
+          case 9:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2, undefined);
+  }));
+
+  return function readAll() {
+    return _ref2.apply(this, arguments);
+  };
+}();
+
+var update = function () {
+  var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3() {
+    var url, readNotes, res, data;
+    return regeneratorRuntime.wrap(function _callee3$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            url = "/.netlify/functions/update";
+            _context3.next = 3;
+            return fetch(url, {
+              body: JSON.stringify({ name: "negative", desc: "Jesus's mother" }),
+              method: "PATCH",
+              "Content-Type": "application/json"
+            });
+
+          case 3:
+            readNotes = _context3.sent;
+            _context3.next = 6;
+            return readNotes.json();
+
+          case 6:
+            res = _context3.sent;
+            data = res.body;
+
+
+            console.log(data);
+
+          case 9:
+          case "end":
+            return _context3.stop();
+        }
+      }
+    }, _callee3, undefined);
+  }));
+
+  return function update() {
+    return _ref3.apply(this, arguments);
+  };
+}();
+
+var deleteOne = function () {
+  var _ref4 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee4() {
+    var url, readNotes, res, data;
+    return regeneratorRuntime.wrap(function _callee4$(_context4) {
+      while (1) {
+        switch (_context4.prev = _context4.next) {
+          case 0:
+            url = "/.netlify/functions/delete";
+            _context4.next = 3;
+            return fetch(url);
+
+          case 3:
+            readNotes = _context4.sent;
+            _context4.next = 6;
+            return readNotes.json();
+
+          case 6:
+            res = _context4.sent;
+            data = res.body;
+
+
+            console.log(data);
+
+          case 9:
+          case "end":
+            return _context4.stop();
+        }
+      }
+    }, _callee4, undefined);
+  }));
+
+  return function deleteOne() {
+    return _ref4.apply(this, arguments);
+  };
+}();
 
 // // create();
 // // readAll();
